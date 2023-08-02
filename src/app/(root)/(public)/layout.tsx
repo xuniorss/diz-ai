ab92d0ec334069260f148e7fa255aef4e2dfd36b
xuniorss <fortunatojunior2020@gmail.com>
2023-08-01 22:15:27 -0300

    feat:initial-config

---
+++ src/app/(root)/(public)/layout.tsx
@@ -0,0 +1,12 @@
+import { SYSTEM_NAME } from '@/system'
+import type { Metadata } from 'next'
+import { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+	title: `${SYSTEM_NAME} - Bem-vindo(a)`,
+	description: 'Lugar onde o seu funcionário tem voz',
+}
+
+export default function PublicLayout({ children }: { children: ReactNode }) {
+	return <div className="h-full">{children}</div>
+}
